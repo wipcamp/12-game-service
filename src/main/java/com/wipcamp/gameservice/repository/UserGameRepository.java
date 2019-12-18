de18153f8bb2550103ca6ee801b5403c9bcea6f8
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-12-18 15:35:47 +0700

    [ADD] UserGameRepository.java

---
+++ src/main/java/com/wipcamp/gameservice/repository/UserGameRepository.java
@@ -0,0 +1,9 @@
+package com.wipcamp.gameservice.repository;
+
+import com.wipcamp.gameservice.model.UserGame;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserGameRepository extends JpaRepository<UserGame,Long> {
+}
