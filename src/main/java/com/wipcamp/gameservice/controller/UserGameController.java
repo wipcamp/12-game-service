509dade16920cec66bca02f9e49e0bd104a5e766
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-12-18 15:34:10 +0700

    [ADD] UserGameController.java

---
+++ src/main/java/com/wipcamp/gameservice/controller/UserGameController.java
@@ -0,0 +1,28 @@
+package com.wipcamp.gameservice.controller;
+
+import com.wipcamp.gameservice.model.UserGame;
+import com.wipcamp.gameservice.service.UserGameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserGameController {
+
+    @Autowired
+    UserGameService service;
+
+    @GetMapping(path = "/profile/{id}")
+    public UserGame getUserGameInformation(@PathVariable int id){
+        return service.findById(id);
+    }
+
+    @GetMapping("/allProfile")
+    public List<UserGame> getAll(){
+        return service.findAll();
+    }
+
+}
