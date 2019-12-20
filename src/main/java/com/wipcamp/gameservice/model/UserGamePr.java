e014fdd8e16e795a0aa401ff48c2a487e07e4170
fame <famedirtyshit@gmail.com>
2019-12-20 15:33:24 +0700

    [ADD] add usergamepr table

---
+++ src/main/java/com/wipcamp/gameservice/model/UserGamePr.java
@@ -0,0 +1,39 @@
+package com.wipcamp.gameservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class UserGamePr {
+
+	@Id
+	@GeneratedValue
+	public String id;
+	public String name;
+	public long highScore;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getHighScore() {
+		return highScore;
+	}
+
+	public void setHighScore(long highScore) {
+		this.highScore = highScore;
+	}
+}
