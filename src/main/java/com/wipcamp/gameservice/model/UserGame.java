bed64a67fb294bb202e8921a701578654eca2343
TipsukonCheer <tipsukon.cheer@mail.kmutt.ac.th>
2019-12-20 14:12:30 +0700

    [ADD] add UserGame

--- src/main/java/com/wipcamp/gameservice/model/UserGame.java
+++ src/main/java/com/wipcamp/gameservice/model/UserGame.java
@@ -6,97 +6,96 @@ import javax.persistence.Id;
 
 @Entity
 public class UserGame {
+	@Id
+	@GeneratedValue
+	private long id;
+	private String team;
+	public int level;
+	public int maxExp;
+	public int exp;
+	public int str;
+	public int dex;
+	public int luk;
+	public int energy;
+	public int maxEnergy;
 
-    @Id
-    @GeneratedValue
-    private long id;
-    private String team;
-    public int level;
-    public int maxExp;
-    public int exp;
-    public int str;
-    public int dex;
-    public int luk;
-    public int energy;
-    public int maxEnergy;
+	public long getId() {
+		return id;
+	}
 
-    public long getId() {
-        return id;
-    }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	public String getTeam() {
+		return team;
+	}
 
-    public String getTeam() {
-        return team;
-    }
+	public void setTeam(String team) {
+		this.team = team;
+	}
 
-    public void setTeam(String team) {
-        this.team = team;
-    }
+	public int getLevel() {
+		return level;
+	}
 
-    public int getLevel() {
-        return level;
-    }
+	public void setLevel(int level) {
+		this.level = level;
+	}
 
-    public void setLevel(int level) {
-        this.level = level;
-    }
+	public int getMaxExp() {
+		return maxExp;
+	}
 
-    public int getMaxExp() {
-        return maxExp;
-    }
+	public void setMaxExp(int maxExp) {
+		this.maxExp = maxExp;
+	}
 
-    public void setMaxExp(int maxExp) {
-        this.maxExp = maxExp;
-    }
+	public int getExp() {
+		return exp;
+	}
 
-    public int getExp() {
-        return exp;
-    }
+	public void setExp(int exp) {
+		this.exp = exp;
+	}
 
-    public void setExp(int exp) {
-        this.exp = exp;
-    }
+	public int getStr() {
+		return str;
+	}
 
-    public int getStr() {
-        return str;
-    }
+	public void setStr(int str) {
+		this.str = str;
+	}
 
-    public void setStr(int str) {
-        this.str = str;
-    }
+	public int getDex() {
+		return dex;
+	}
 
-    public int getDex() {
-        return dex;
-    }
+	public void setDex(int dex) {
+		this.dex = dex;
+	}
 
-    public void setDex(int dex) {
-        this.dex = dex;
-    }
+	public int getLuk() {
+		return luk;
+	}
 
-    public int getLuk() {
-        return luk;
-    }
+	public void setLuk(int luk) {
+		this.luk = luk;
+	}
 
-    public void setLuk(int luk) {
-        this.luk = luk;
-    }
+	public int getEnergy() {
+		return energy;
+	}
 
-    public int getEnergy() {
-        return energy;
-    }
+	public void setEnergy(int energy) {
+		this.energy = energy;
+	}
 
-    public void setEnergy(int energy) {
-        this.energy = energy;
-    }
+	public int getMaxEnergy() {
+		return maxEnergy;
+	}
 
-    public int getMaxEnergy() {
-        return maxEnergy;
-    }
-
-    public void setMaxEnergy(int maxEnergy) {
-        this.maxEnergy = maxEnergy;
-    }
+	public void setMaxEnergy(int maxEnergy) {
+		this.maxEnergy = maxEnergy;
+	}
 }
