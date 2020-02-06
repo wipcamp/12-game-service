ee5c08ec127d78c5029b226213c0a6a6bfbe273b
fame <famedirtyshit@gmail.com>
2020-02-06 19:14:32 +0700

    [UPDATE] add cross origin in gamrPr controller

--- src/main/java/com/wipcamp/gameservice/controller/UserGamePrController.java
+++ src/main/java/com/wipcamp/gameservice/controller/UserGamePrController.java
@@ -4,6 +4,7 @@ import com.wipcamp.gameservice.model.UserGamePr;
 import com.wipcamp.gameservice.service.UserGamePrService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 public class UserGamePrController {
 
