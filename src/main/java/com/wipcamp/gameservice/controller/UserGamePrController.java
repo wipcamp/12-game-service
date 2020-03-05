5b67ca3e5d687d0c9edf589cd730e0b313a276d5
fame <famedirtyshit@gmail.com>
2020-03-05 12:56:43 +0700

    [UPDATE] test cross origin

--- src/main/java/com/wipcamp/gameservice/controller/UserGamePrController.java
+++ src/main/java/com/wipcamp/gameservice/controller/UserGamePrController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.List;
 
-@CrossOrigin("https://12-gamepr.freezer.wip.camp")
+@CrossOrigin("https://wip.camp")
 @RestController
 public class UserGamePrController {
 
